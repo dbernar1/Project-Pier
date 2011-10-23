f4322c09ed89134b71f53cad470ea9971387122d
Dan Bernardic <dan@bernardic.ca>
2011-10-22 23:53:22 -0500

    If the system is not installed and the user goes to /, the system automatically redirects them to /public/install.

--- init.php
+++ init.php
@@ -45,7 +45,8 @@
   // If script is not installed config.php will return false. Otherwise it will
   // return NULL. If we get false redirect to install folder
   if (!@include_once(ROOT . '/config/config.php')) {
-    print "ProjectPier is not installed. Please redirect your browser to <b><a href=\"./". PUBLIC_FOLDER . "/install\">" . PUBLIC_FOLDER . "/install</a></b> folder and follow installation procedure";
+    header('HTTP/1.1 302 Found');
+    header('Location: '.PUBLIC_FOLDER.'/install');
     die();
   } // if
 
